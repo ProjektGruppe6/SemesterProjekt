9d419d6a68e84efc7823493287fbdc13c8a1b669
Abraham <abli@10.126.90.197>
2018-10-14 21:16:33 +0200

    Update SemesterProject.java

--- src/worldofzuul/SemesterProjekt.java
+++ src/worldofzuul/SemesterProjekt.java
@@ -16,6 +16,9 @@ public class SemesterProjekt {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Game game = new Game();
+        game.play();
     }
     
 }
