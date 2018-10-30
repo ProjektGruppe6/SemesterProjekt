8024481475731a408689602e5f5d0e8f3162d0e7
Abraham <abli@10.126.90.197>
2018-10-30 12:28:05 +0100

    Item Class updating

---
+++ src/worldofzuul/Item.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author abli
+ */
+class Item {
+    
+    String description;
+    
+    public Item(String newdescription){
+        description = newdescription;
+    
+    }
+    public String getDescription(){
+    
+        return description;
+    }
+    
+}
