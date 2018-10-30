e917d47117818ecb8f28fb74c829448a14529c77
Abraham <abli@10.126.90.197>
2018-10-30 12:27:35 +0100

    add New Class( task.java) Updating Game Class. updating Rooom Class.

--- src/worldofzuul/CommandWord.java
+++ src/worldofzuul/CommandWord.java
@@ -2,7 +2,7 @@ package worldofzuul;
 
 public enum CommandWord
 {
-    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
+    GO("go"), QUIT("quit"), HELP("help"), INVENTORY("inventory"), GET("get"), UNKNOWN("?");
     
     private String commandString;
     
