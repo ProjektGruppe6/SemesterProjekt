a3f9f4c5fd96252fbdcfd33a73fd8c518fbb6374
Abraham <abli@10.126.90.197>
2018-10-30 12:22:50 +0100

    add New Class( task.java) Updating Game Class. updating Rooom Class.

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -1,22 +1,60 @@
 package worldofzuul;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class Game 
 {
     private Parser parser;
     private Room currentRoom;
-        
-
+    Room outside, semesterProject, objectOriented, softwareEngeneering, computersystem,projektlokal, onlineCourse, bar, library, cafe, secondSemester, Task ;
+    ArrayList<Item> inventory = new ArrayList<Item>();
+    
     public Game() 
     {
         createRooms();
         parser = new Parser();
     }
 
+    
+private void createTask(){
+    
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(" "));
+                String line;
+                while((line = br.readLine())!=null) 
+                {
+                // process the line.
+                    String[] temp = line.split(";");
+                    if(temp[0].equals("OP"))
+                    {
+                        objectOriented.addTask(new Task(temp[1],temp[3],temp[2]));
+                    }
+                        
+                }
+                    
+                    
+                    
+                    
+        } catch (Exception ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+   
+
+}    
+    
+    
 
 private void createRooms()
     {
-        Room outside, semesterProject, objectOriented, softwareEngeneering, computersystem,projektlokal, onlineCourse, bar, library, cafe, secondSemester ;
- 
+       
         outside = new Room("outside the main entrance of the university");
         onlineCourse = new Room("in online course class");
         semesterProject = new Room("in Group local rooms with your group members");
@@ -38,7 +76,17 @@ private void createRooms()
         softwareEngeneering.setExit("south",outside);
         softwareEngeneering.setExit("east", onlineCourse);
         softwareEngeneering.setExit("west", semesterProject);
-       
+      
+        
+        
+        
+        //   public void addTask(Task task){
+       // tasks.add(task);
+        
+    //public void setExit(String direction, Room neighbor) {
+      //  exits.put(direction, neighbor);
+        
+        
         
         semesterProject.setExit("north", library);
         semesterProject.setExit("south", objectOriented);
@@ -50,6 +98,8 @@ private void createRooms()
         objectOriented.setExit("south", cafe);
         objectOriented.setExit("east", outside);
         objectOriented.setExit("west", library);
+       
+        
         
         onlineCourse.setExit("north", bar);
         onlineCourse.setExit("south", computersystem);
@@ -60,7 +110,12 @@ private void createRooms()
         computersystem.setExit("south", cafe);
         computersystem.setExit("east", library);
         computersystem.setExit("west", outside);
+        inventory.add(new Item("Computer"));
         
+        objectOriented.setItem(new Item("Robot"));
+        computersystem.setItem(new Item("Robot"));
+        onlineCourse.setItem(new Item("Robot"));
+        softwareEngeneering.setItem(new Item("Robot"));
 
         currentRoom = outside;
     }
@@ -99,14 +154,23 @@ private void createRooms()
             return false;
         }
 
-        if (commandWord == CommandWord.HELP) {
-            printHelp();
-        }
-        else if (commandWord == CommandWord.GO) {
-            goRoom(command);
-        }
-        else if (commandWord == CommandWord.QUIT) {
-            wantToQuit = quit(command);
+        if (null != commandWord) switch (commandWord) {
+            case HELP:
+                printHelp();
+                break;
+            case GO:
+                goRoom(command);
+                break;
+            case QUIT:
+                wantToQuit = quit(command);
+                break;
+            case INVENTORY:
+                printInventory();
+                break;
+            
+                
+            default:
+                break;
         }
         return wantToQuit;
     }
@@ -150,4 +214,15 @@ private void createRooms()
             return true;
         }
     }
+
+    private void printInventory() {
+        String output = ("");
+        for (int i = 0; i < inventory.size(); i++) {
+          output += inventory.get(i).getDescription() + " " ;
+        }
+        System.out.println("you are Carrying :");
+        System.out.println(output);
+    }
 }
+
+
